c0d9f90011d71ca039d26a23243d0eb301c15dd1
Bartek Sosin <bscode03@gmail.com>
2021-01-16 19:13:42 +0100

    Fix calendar.js

--- calendar.js
+++ calendar.js
@@ -3,32 +3,29 @@ const weekButton = document.querySelector('.week');
 const day = document.querySelector('.dayContainer');
 const week = document.querySelector('.weekContainer');
    
-var number;
 
-function changeNumber(n){
-        
-        dayButton.addEventListener('click', () =>{
-            n = 1 
-        }); 
-        weekButton.addEventListener('click', () =>{
-            n = 2;
-            console.log(n)
-        })
-    }
 
-    changeNumber(number);
-
-    if(number = 1){
-        day.classList.add('appear')
-        week.classList.add('dissapear')
-    }
+dayButton.addEventListener('click', () => {
+    day.classList.add('appear')
+    week.classList.remove('appear')
+})
     
-    else if(number = 2){
-        day.classList.remove('appear')
-        day.classList.add('dissapear')
-        week.classList.remove('dissapear')
-        week.classList.add('appear')
-    }
+weekButton.addEventListener('click', () => {
+    day.classList.remove('appear')
+    week.classList.add('appear')
+})
+
+  
+
+ 
+
+
+
+
+       
+
+
+   
 
 
 
