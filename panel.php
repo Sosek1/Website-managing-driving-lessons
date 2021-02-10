5b6328e0d99c75cba869ff5c2abf2e162b3c2b3c
Ktechnik <ktechnik@interia.pl>
2021-02-10 18:12:51 +0100

    select vehicles

--- panel.php
+++ panel.php
@@ -51,39 +51,84 @@ if(!isset($_SESSION['logIn'])){
                 <input type="text" class="searchInput" name="szukanie">
                 <label><input type="submit"value=""style= "border-style:none;"><i class="fas fa-search"></i></label>
         </div>
-            <input type="text" name="name" class="name2 border " placeholder="Imię...">
-            <input type="text" name="surname" class="surname border " placeholder="Nazwisko...">
-            <input type="text" name="nrtel" class="phoneNumber border " placeholder="Numer telefonu...">
+            <input type="text" name="addname" class="name2 border " placeholder="Imię..."<?php if(isset($_SESSION['adname'])){echo 'value="'.$_SESSION['adname'].'"';}?>>
+            <input type="text" name="addsurname" class="surname border " placeholder="Nazwisko..."<?php if(isset($_SESSION['adsurname'])){echo 'value="'.$_SESSION['adsurname'].'"';}?>>
+            <input type="text" name="addnrtel" class="phoneNumber border " placeholder="Numer telefonu..."<?php if(isset($_SESSION['adnrtel'])){echo 'value="'.$_SESSION['adnrtel'].'"';}?>>
             <div class="chooseCategory">
                 <p class="text">Kategoria</p>
-                <select class="custom-select" name="kat">
-                    <option value="1">AM</option>
-                    <option value="2">A1</option>
-                    <option value="3">A2</option>
-                    <option value="4">A</option>
+                <select class="custom-select" name="addkat">
+                    <option value="1"<?php if(isset($_SESSION['adkat'])){if($_SESSION['adkat']==1){echo 'selected="selected"';}}?>>AM</option>
+                    <option value="2"<?php if(isset($_SESSION['adkat'])){if($_SESSION['adkat']==2){echo 'selected="selected"';}}?>>A1</option>
+                    <option value="3"<?php if(isset($_SESSION['adkat'])){if($_SESSION['adkat']==3){echo 'selected="selected"';}}?>>A2</option>
+                    <option value="4"<?php if(isset($_SESSION['adkat'])){if($_SESSION['adkat']==4){echo 'selected="selected"';}}?>>A</option>
                 </select>
             </div>
             <div class="chooseHour">
                 <p class="text">Liczba godzin</p>
-                <select class="custom-select">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
+                <select class="custom-select" name="addhours">
+                    <option value="1"<?php if(isset($_SESSION['adhours'])){if($_SESSION['adhours']==1){echo 'selected="selected"';}}?>>1</option>
+                    <option value="2"<?php if(isset($_SESSION['adhours'])){if($_SESSION['adhours']==2){echo 'selected="selected"';}}?>>2</option>
+                    <option value="3"<?php if(isset($_SESSION['adhours'])){if($_SESSION['adhours']==3){echo 'selected="selected"';}}?>>3</option>
                 </select>
             </div>
             <div class="chooseCar">
                 <p class="text"> Pojazd</p>
-                <select class="custom-select">
-                    <option value="1">AM</option>
-                    <option value="2">A1</option>
-                    <option value="3">A2</option>
-                    <option value="4">A</option>
+                <select class="custom-select" name="addpojazd">
+                    <?php
+                    $conn = @new mysqli($host, $db_user, $db_pass, $db_name);
+                    if(isset($_SESSION['adid'])){
+
+                        if($conn->connect_errno!=0){
+
+                        }else{
+                            $id = $_SESSION['adid'];
+                           // $osoba=$conn->query("SELECT * FROM kursanci WHERE id='$id'");
+
+                            if(!$osoba){}else{
+                                $ile=$rezu->num_rows;
+                                if($ile>0){
+                                    $osobarow = $osoba->fetch_assoc();
+                                    $katosoba = $osobarow['kat'];
+                                }
+                            }
+                        }
+                    }
+                    if(!isset($katosoba)){
+                        $katosoba=5;
+                    }
+
+                    $rezu=$conn->query("SELECT * FROM pojazdy WHERE kat<='$katosoba'");
+
+                    if(!$rezu){}else{
+                        $ile=$rezu->num_rows;
+                        if($ile>0){
+                            $i = 1;
+                            while($i<=$ile){
+                                $pojazdrow = $rezu->fetch_assoc();
+                                $pojname = $pojazdrow['nazwa'];
+                                if(isset($_SESSION['adkat'])){
+                                    if($_SESSION['adkat']==$i){
+                                        echo '<option value='.$i.'selected="selected">'.$pojname.'</option>';
+                                    }else{
+                                        echo '<option value='.$i.'>'.$pojname.'</option>';
+                                    }
+                                }else{
+                                    echo '<option value='.$i.'>'.$pojname.'</option>';
+                                }
+
+                                $i++;
+                            }
+                                
+                        }
+                        $conn -> close();       
+                    }
+                    ?>
                 </select>
             </div>
-            <div class="city">miasto</div>
-            <div class="place">plac</div>
-            <div class="vehicle">miasto/plac</div>
-            <textarea class="info" placeholder="Napisz coś..."></textarea>
+            <div class="city" name="addmiasto">miasto</div>
+            <div class="place" name="addplac">plac</div>
+            <div class="vehicle" name=addmspl>miasto/plac</div>
+            <textarea class="info" placeholder="Napisz coś..." name="addinfo"></textarea>
             <button type="submit" class="save">zapisz</button>
     </form>
 
