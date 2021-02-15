47789c52b36951210287e20588a81427454e3d85
Ktechnik <ktechnik@interia.pl>
2021-02-15 17:06:52 +0100

    add delete data in panel

---
+++ delete_date.php
@@ -0,0 +1,14 @@
+<?php
+unset($_SESSION['d']);
+unset($_SESSION['h']);
+unset($_SESSION['adid']);
+unset($_SESSION['adname']);
+unset($_SESSION['adsurname']);
+unset($_SESSION['adnrtel']);
+unset($_SESSION['adkat']);
+unset($_SESSION['adhours']);
+unset($_SESSION['adpojazd']);
+unset($_SESSION['adplace']);
+unset($_SESSION['adinfo']);
+header('Location: panel.php');
+?>
