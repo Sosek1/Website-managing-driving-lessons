59ab4fa02a56a7bcaedfecf7602b8e89ba5d458f
Ktechnik <ktechnik@interia.pl>
2021-02-18 13:23:59 +0100

    editing i deleting jazdy

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once "connect.php";
+if(!isset($_SESSION['logIn'])){
+    header('Location: index.php');
+    exit();
+}
+if(isset($_GET['id'])){
+    $id_jazdy = $_GET['id'];
+
+}
+$conn = @new mysqli($host, $db_user, $db_pass, $db_name);
+$conn->query("SET NAMES 'utf8'");
+if($conn->connect_errno!=0){
+    $con=false;
+}else{
+    $zap = 'DELETE FROM jazdy WHERE id="'.$id_jazdy.'"';
+    $rezu=$conn->query($zap);
+    if(!$rezu){
+    }else{
+    }
+}
+$conn->close();
+header('Location: kalendarzDzien.php');
+
+
+
+?>
