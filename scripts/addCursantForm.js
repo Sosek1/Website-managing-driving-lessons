7ad2ac69fdc23e6ad9ef5b5f30fc04ce1e13a9e7
Bartek Sosin <bscode03@gmail.com>
2021-06-13 21:28:44 +0200

    another changes

--- scripts/addCursantForm.js
+++ scripts/addCursantForm.js
@@ -1,12 +1,14 @@
-const form = document.getElementById("addCursant");
+        const form = document.getElementById("addCursant");
         const container = document.getElementById("container");
         const heading = document.getElementById("adminHeading");
         const button = document.getElementById("btn");
+        const form2 = document.getElementById("changePassword")
         let show = false;
         const toBlur = [container, heading, button]
         const showForm = () => {
             if (!show) {
                 form.style.display = "flex";
+                // form2.style.display = "flex";
                 toBlur.forEach(element => {
                     element.style.filter = "blur(4px)"
                 });
@@ -16,6 +18,7 @@ const form = document.getElementById("addCursant");
         const closeForm = () => {
             if (!show) {
                 form.style.display = "none";
+                // form2.style.display = "none";
                 toBlur.forEach(element => {
                     element.style.filter = "blur(0)"
                 });
